6ac31a295af205471fead989a2961941a9866ff5
amedeo463 <poppydasecond@protonmail.com>
2025-06-28 18:50:43 +0200

    Made a .gitignore for later

--- commit.sh
+++ commit.sh
@@ -1,11 +1,6 @@
-#! /bin/sh
+# Script I use to commit
+# please ignore
 
-# Script I use to commit and push, please ignore
-
-# Tidy up repo
-make fullcleanup
-
-# Commit and push (add repo first)
 git add .
 git commit
 git push
